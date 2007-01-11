6f4e2246f3f3eba2361325366fbc73ccd3640b44
alberto <alberto@d8cf2a8c-14c1-412e-b3b3-5d50375898f8>
2007-01-11 12:08:27 +0000

    Let's play the safe side
    git-svn-id: http://172.26.0.2/svn/sudoku/mpl/li/python/p2@26 d8cf2a8c-14c1-412e-b3b3-5d50375898f8

--- Sudoku.py
+++ Sudoku.py
@@ -24,6 +24,34 @@
 #
 #    Please see the file COPYING for the full license.
 #
+# -----------------------------------------------------------------------------
+# ORIGINAL LICENSE FOR tkSudoku.py - Revision 49 and Revision 52
+#
+# Only The original pieces of code submitted under Revisions 49 and 52
+# are under the next License. The revision numbers may change when the
+# code is moved from the MPL private repository to the projects public
+# one. (Current Address of this file $HeadURL$)
+#
+# Copyright (c) 2006 Juan Manuel Gimeno Illa
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """A Sudoku Solver"""
 
